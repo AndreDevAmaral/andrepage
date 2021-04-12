96369fbc04b791dbdb5240307b65e50e1f112dfd
AndreDevAmaral <andre.dev.amaral@gmail.com>
2021-04-12 16:35:20 -0300

    adicionar produtos

---
+++ pages/api/produtos/[id].js
@@ -0,0 +1,3 @@
+export default function Produtos(request, reponse) {
+    console.log(request.query.id);
+}
--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,10 +1,16 @@
-function tempo(request, reponse){
+function tempo(request, reponse) {
     const dynamicDate = new Date();
 
+    const a = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const b = await a.json();
+    const c = b.userId;
+
+    //Cache de 10 segundos
     reponse.setHeader('Cache-Control', 's-maxage=10', 'state-while-revalidate');
 
     reponse.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        id: c
     });
 }
 
